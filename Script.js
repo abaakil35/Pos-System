84fdecb8e75e488b28f8f777f1b517c16fa846b3
Abaakil <154565344+Abaakil35@users.noreply.github.com>
2024-10-31 00:52:55 +0100

    not Yet !!!

--- Script.js
+++ Script.js
@@ -1,19 +1,24 @@
-const panies = document.getElementById('panies');
-const cart_title = document.querySelector('.cart-title');
+// const panies = document.getElementById('panies');
+// const cart_title = document.querySelector('.cart-title');
+// const body_page = document.getElementById('body');
+// const payes = document.getElementById('panierSidebar');
 
+// var isShowed = false;
 
-const payes = document.getElementById('panierSidebar');
+// panies.addEventListener('click', () => {
+//     if (isShowed) {
+//         payes.style.right = '-100%';
+//         body_page.style.filter = 'blur(0px)';
+//         isShowed = false;
+//     } else {
+//         payes.style.right = '0%';
+//         body_page.style.filter = 'blur(3px)';
+//         isShowed = true;
+//     }
+// });
 
-var isShowed = true
-panies.addEventListener('click',()=>{
-    if(!isShowed){
-        payes.style.right = '-100%';
-        isShowed = true;
-    }else {
-        payes.style.right = '0%';
-        isShowed = false;
-    }
-})
-cart_title.addEventListener('click',function(){
-    payes.style.right = '-100%';
-})
+// cart_title.addEventListener('click', function() {
+//     payes.style.right = '-100%';
+//     body_page.style.filter = 'blur(0px)';
+//     isShowed = false;
+// });
